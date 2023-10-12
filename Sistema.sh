e5aa7d7d9c3490dd4d7b34bf7ab6d45f765d48f4
SiriusB <80565063+gideeon13@users.noreply.github.com>
2023-10-12 18:45:54 -0300

    Update Sistema.sh

--- Sistema.sh
+++ Sistema.sh
@@ -2,6 +2,46 @@
 
 \e]2;Sistema de Relevamiento de Salas de Informática\a
 
+# Función para Ingresar al Sistema
+
+function ingresar_sistema() {
+  clear
+  echo " __________________________________________________"
+  echo "|                   Bienvenido al                  |"
+  echo "| Sistema de Relevamiento de Salas de Informática  |"
+  echo "|__________________________________________________|"
+  read -p "Ingrese el nombre de usuario: " usuario
+  read -s -p "Ingrese la contraseña: " password
+  echo
+}
+
+  # Verificar las credenciales de Usuario
+   if autenticar_usuario "$usuario" "$password"; then
+     if [ "$usuario" == "root" ]; then
+       menu_root
+     else 
+       menu_usuario "$usuario"
+     fi
+    else
+      echo "Credenciales incorrectas."
+    fi
+
+# Función para autenticar un usuario
+function autenticar_usuario() {
+  local usuario="$1"
+  local password="$2"
+
+  # Buscar las credenciales del usuario en el archivo de contraseñas
+  if grep -q "^$usuario:" "$ARCHIVO_CONTRASEÑAS"; then
+    hashed_password=$(grep "^$usuario:" "$ARCHIVO_CONTRASEÑAS" | cut -d: -f2)
+    if bcrypt -s "$password" "$hashed_password"; then
+      echo "Autenticacion exitosa."
+      return 0
+    fi
+  fi
+  return 1
+}
+
 # Menú para el usuario root (Administrador)
 function menu_root() {
   while true; do
